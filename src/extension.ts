f04332867e14c4ed12f969a46ca8e0dd2c3b8a46
josephsaviokav <josephsaviokav@gmail.com>
2025-12-22 19:47:34 +0530

    removed the comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,33 +1,20 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "google-search" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('google-search.googleSearch', async () => {
 
 		const query = await vscode.window.showInputBox({ prompt: 'Enter your Google search query' });
 		if(query ==null) {
-			return; // User cancelled the input
+			return;
 		}
 		const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
 		vscode.env.openExternal(vscode.Uri.parse(url));
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		// vscode.window.showInformationMessage('Hello World from google search!');
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
