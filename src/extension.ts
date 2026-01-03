af7a2179e65c878916e080c53c1888d80732e156
josephsaviokav <josephsaviokav@gmail.com>
2026-01-03 18:55:52 +0530

    updated the extensions file to include search engines lile brave and bing

--- src/extension.ts
+++ src/extension.ts
@@ -2,16 +2,51 @@ import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
 
-	console.log('Congratulations, your extension "google-search" is now active!');
+	console.log('Congratulations, your extension "quick-search" is now active!');
 
-	const disposable = vscode.commands.registerCommand('google-search.googleSearch', async () => {
-
-		const query = await vscode.window.showInputBox({ prompt: 'Enter your Google search query' });
+	const disposable = vscode.commands.registerCommand('google-search.quickSearch', async () => {
+		const val=await vscode.window.showInputBox({prompt:'Enter your preferred search engine (google,bing,edge,brave)'});
+		if(val==null){
+			return;
+		}
+		let engine=val.toLowerCase();
+		if(engine!=='google' && engine!=='bing' && engine!=='brave'){
+			vscode.window.showErrorMessage('Unsupported search engine. Please enter google, bing, or brave.');
+			return;
+		}
+		if(engine==='google'){
+			const query = await vscode.window.showInputBox({ prompt: 'Enter your search query' });
 		if(query ==null) {
 			return;
 		}
 		const url = `https://www.google.com/search?q=${encodeURIComponent(query)}`;
 		vscode.env.openExternal(vscode.Uri.parse(url));
+		}
+		else if(engine==='bing'){
+			const query = await vscode.window.showInputBox({ prompt: 'Enter your search query' });
+		if(query ==null) {
+			return;
+		}
+		const url = `https://www.bing.com/search?q=${encodeURIComponent(query)}`;
+		vscode.env.openExternal(vscode.Uri.parse(url));
+		}
+		// else if(engine==='edge'){
+		// const query = await vscode.window.showInputBox({ prompt: 'Enter your search query' });
+		// if(query ==null) {
+		// 	return;
+		// }
+		// const url = `https://www.bing.com/search?q=${encodeURIComponent(query)}`;
+		// vscode.env.openExternal(vscode.Uri.parse(url));
+		// }
+		else if(engine==='brave'){
+			const query = await vscode.window.showInputBox({ prompt: 'Enter your search query' });
+		if(query ==null) {
+			return;
+		}
+		const url = `https://search.brave.com/search?q=${encodeURIComponent(query)}`;
+		vscode.env.openExternal(vscode.Uri.parse(url));
+	}
+
 	});
 
 	context.subscriptions.push(disposable);
